913527daecea094f1b0832ca8e651d2b03c4b61a
Chung Tran van <chungtranvan@Chungs-MacBook-Air.local>
2022-03-24 14:47:21 +0900

    new file lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+  let mut x: i32 = 10; // mut = muttable co the bien doi
+  println!("x={}",x);
+  x = 20;
+  println!("x = {}",x);
+
+  const HANG_SO: u128 = 100_000_000_000_000_000_000;
+  println!("HANG_SO= {}", HANG_SO);
 }
